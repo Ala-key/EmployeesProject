221663567c4b81c3cd5b2a660b8e56a68495b299
Ala-key <almir.abdrasulov@mail.ru>
2023-07-21 14:32:56 +0600

    Created class EmployeeManagement and created methods

--- EmployeesProject/Program.cs
+++ EmployeesProject/Program.cs
@@ -17,6 +17,40 @@ namespace EmployeesProject
         public string Position { get; set; }
     }
 
+    
+
+    public class EmployeeManagement
+    {
+        private List<Employee> list = new List<Employee>();
+
+        public void AddEmployee(Employee employee)
+        {
+            if (employee == null)
+                return;
+            list.Add(employee);
+        }
+
+
+        public void RemoveEmployee(int id)
+        {
+            var emp = list.FirstOrDefault(x => x.id == id);
+            if (emp == null)
+                return;
+            list.Remove(emp);
+        }
+
+
+        public List<Employee> GetAllEmployee()
+        {
+            if (list == null)
+            {
+                return new List<Employee>();
+            }
+
+            return list;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
