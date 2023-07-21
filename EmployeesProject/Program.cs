0e7d013f5c77362826a1c7d632631bcddb87f4c5
Ala-key <almir.abdrasulov@mail.ru>
2023-07-21 14:18:16 +0600

    Created employee class

--- EmployeesProject/Program.cs
+++ EmployeesProject/Program.cs
@@ -8,8 +8,15 @@ namespace EmployeesProject
 {
     public class Employee
     {
-        
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Position { get; set; }
     }
+
     internal class Program
     {
         static void Main(string[] args)
